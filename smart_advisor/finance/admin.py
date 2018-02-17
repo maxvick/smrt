6cc02626d28d23a99e5e82df93842e2589e6474e
maxvick <max.vickerson@gmail.com>
2018-02-17 12:13:03 +1000

    removed finance app

--- smart_advisor/finance/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here for finance
-#from .models import Account
-#from .models import Transaction
-
-#admin.site.register(Account)
-#admin.site.register(Transaction)
