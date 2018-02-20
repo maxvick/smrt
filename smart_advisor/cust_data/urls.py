a186c0d9984b5177ba2f13c0c1db63a17f3dd429
maxvick <max.vickerson@gmail.com>
2018-02-20 22:24:15 +1000

    added an extra URL in cd for /accounts/profile login is supposed to redirect there but it doesn't work will need to declare the login as a view and update the template & next variable

--- smart_advisor/cust_data/urls.py
+++ smart_advisor/cust_data/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile', views.not_yet, name='tbd'),
 ]
